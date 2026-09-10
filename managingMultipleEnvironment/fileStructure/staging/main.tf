feb8113c0b68918173bdde583aa58c53cffd4900
velarina <velarina2014@gmail.com>
2026-09-10 14:17:21 +0700

    Fix terraform formatting

--- managingMultipleEnvironment/fileStructure/staging/main.tf
+++ managingMultipleEnvironment/fileStructure/staging/main.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "molina-ts-state"
-    key = "managinMultipleEnvironment/fileStructure/staging/terraform.tfstate"
-    region = "ap-southeast-1"
+    bucket         = "molina-ts-state"
+    key            = "managinMultipleEnvironment/fileStructure/staging/terraform.tfstate"
+    region         = "ap-southeast-1"
     dynamodb_table = "terraform-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
@@ -21,8 +21,8 @@ provider "aws" {
 
 variable "database_password" {
   description = "database password"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 locals {
@@ -30,14 +30,14 @@ locals {
 }
 
 module "web_app" {
-    source = "../../../organizationAndModules/web-app-module"
+  source = "../../../organizationAndModules/web-app-module"
 
-    bucket_prefix = "web-app-data-${local.environment_name}"
-    domain = "sendiko.dev"
-    environment_name = local.environment_name
-    instance_type = "t3.micro"
-    create_dns_zone = false
-    database_name = "${local.environment_name}mydb"
-    database_user = "foo"
-    database_password = var.database_password
+  bucket_prefix     = "web-app-data-${local.environment_name}"
+  domain            = "sendiko.dev"
+  environment_name  = local.environment_name
+  instance_type     = "t3.micro"
+  create_dns_zone   = false
+  database_name     = "${local.environment_name}mydb"
+  database_user     = "foo"
+  database_password = var.database_password
 }
